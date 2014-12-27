73fb0f0481d1c343c433001b7065aef238d20cb2
Delirium <daniel.castillo00@gmail.com>
2014-12-26 22:55:36 -0600

    Se elimina el catalogo de promociones

--- inna-sinai-web/src/main/java/com/inna/sinai/web/view/controller/CommonController.java
+++ inna-sinai-web/src/main/java/com/inna/sinai/web/view/controller/CommonController.java
@@ -6,7 +6,6 @@ import com.inna.sinai.common.controller.AbstractController;
 import com.inna.sinai.common.service.core.CatalogTemplateService;
 import com.inna.sinai.web.service.catalog.JobSpecService;
 import com.inna.sinai.web.service.catalog.JobZoneService;
-import com.inna.sinai.web.service.catalog.PromotionService;
 
 @SessionAttributes({"session"})
 public class CommonController extends AbstractController {
@@ -14,7 +13,6 @@ public class CommonController extends AbstractController {
   protected CatalogTemplateService ctService = null;
   protected JobZoneService jzService = null;
   protected JobSpecService jsService = null;
-  protected PromotionService pService = null;
 
   public void setCtService(CatalogTemplateService ctService) {
 	this.ctService = ctService;
@@ -26,10 +24,6 @@ public class CommonController extends AbstractController {
 
   public void setJsService(JobSpecService jsService) {
 	this.jsService = jsService;
-  }
-
-  public void setpService(PromotionService pService) {
-	this.pService = pService;
   }
 
 }
